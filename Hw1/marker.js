c861fe40dfc0cc9a78768ae39c53d7b2cb26c3e3
bkhadka2 <bishalkhadka1600@gmail.com>
2019-01-29 13:28:30 -0700

    made changes to this folder

--- Hw1/marker.js
+++ Hw1/marker.js
@@ -1,5 +1,10 @@
 "use strict";
-
+class BadMarkerState
+{
+    constructor(problem){
+        this._problem = problem;
+    }
+}
 class Marker
 {
     constructor(color)
@@ -10,7 +15,15 @@ class Marker
 
     draw()
     {
-        console.log("drawing in "+ this._color);
+        if(! this._capped)
+        {
+            console.log("drawing in "+ this._color);
+        }
+        else
+        {
+            throw new BadMarkerState("drawing with capped marker is bad");
+        }
+         
     }
 
     get color()
@@ -22,6 +35,23 @@ class Marker
     {
         this._color = value;
     }
+
+    get capped()
+    {
+        return this._capped;
+    }
+    
+    set capped(value)
+    {
+        if(typeof value =="boolean")
+        {
+            this._capped = value;
+        }
+        else
+        {
+            throw new BadMarkerState("Capped must be true or false");
+        }
+    }
 }
 
 var blackMarker = new Marker("black");
@@ -29,5 +59,10 @@ var redMarker = new Marker ("red");
 
 blackMarker.draw();//bad idea
 redMarker.draw();
+
+redMarker.capped = false;
+console.log('I have a ' + blackMarker.color + 'Marker');
+
+
 
 
