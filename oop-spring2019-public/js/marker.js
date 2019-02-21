7b8dd88f6ba38bb51df02bac52a5f03f82eac760
bkhadka2 <bishalkhadka1600@gmail.com>
2019-02-21 13:22:18 -0700

    Added inheritance in js

--- oop-spring2019-public/js/marker.js
+++ oop-spring2019-public/js/marker.js
@@ -6,16 +6,48 @@ class BadMarkerState {
     }
 }
 
-class Marker {
+//abstract 
+class writingUtensil
+{
     constructor(color) {
-        console.log("constructed marker");
+        console.log("constructing writing utensil");
         this._color = color;
-        this._capped = true;
     }
 
-    draw() {
+    //abstract
+    write() {
+        throw new Error("Write is abstract");
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    get capped() {
+        return this._capped;
+    }
+
+    set capped(value) {
+        if (typeof value == "boolean") {
+            this._capped = value;
+        } else {
+            throw Marker.BAD_MARKER_CAPPED_INVALID;
+        }
+    }
+
+}
+
+class Marker extends writingUtensil{
+    constructor(color) {
+        super(color);
+        console.log("constructing marker");
+        this._capped = true;
+    }
+    // changing the definition (override) since we are making some change in write() method in Marker.
+    // Not an override if you use different method in derived classes than that of base classes.
+    write() {
         if (!this._capped) {
-           console.log("drawing in " + this._color);
+           console.log("writing in " + this.color);
         } else {
             throw Marker.BAD_MARKER_CAPPED_DRAW;
         }
