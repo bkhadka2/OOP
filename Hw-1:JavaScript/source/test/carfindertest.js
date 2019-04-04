c83ca6c7b3ae4d89479cbd2f589e26b623445aef
bkhadka2 <bishalkhadka1600@gmail.com>
2019-04-03 21:09:31 -0600

    Added hw-1 Javascript

---
+++ Hw-1:JavaScript/source/test/carfindertest.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var assert = require("chai").assert;
+var carfinder = require("../carfinder");
+
+describe("CarFinder", function(){
+    it("CarFinder should output Dodge", function(){
+        console.log("CarFinder:", CarFinder.Options)
+    })
+})
