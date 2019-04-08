99a74e048c53bec9d0f072b5c3924ed589a1f31b
bkhadka2 <bishalkhadka1600@gmail.com>
2019-04-08 11:52:59 -0600

    Added assertion testing

--- Hw-3:C++/source/main.cpp
+++ Hw-3:C++/source/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <assert.h>
 #include "../header/bank.h"
 
 
@@ -8,6 +9,14 @@ using namespace std;
 
 int main(int argc, const char ** argv)
 {
+    // Assertion Testing 
+    // Testing if the person's balance is less than 0;
+    // If the person's balance is less than zero,program will terminate else continue.
+    Bank obj(-100, "Bishal", 100);
+    int temp = obj.getBalance();
+    assert(temp >= 0);
+    cout << "Test Passed.. You don't have a negative balance" << endl;
+
     shared_ptr<Bank> ptr = make_shared<Bank> ();
     ptr->setName();
     ptr->setBalance();
@@ -26,5 +35,7 @@ int main(int argc, const char ** argv)
     }
     ptr->LoanMoney();
     ptr->displayInfo();
+
+    
     return 0;
 }
