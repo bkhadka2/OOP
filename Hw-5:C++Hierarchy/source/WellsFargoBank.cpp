fdaa10aad55422434d1e5a5c4ae6fe950e72b415
bkhadka2 <bishalkhadka1600@gmail.com>
2019-05-08 23:31:48 -0600

    Hw-5:C++Hierarchy added

--- Hw-5:C++Hierarchy/source/WellsFargoBank.cpp
+++ Hw-5:C++Hierarchy/source/WellsFargoBank.cpp
@@ -7,11 +7,11 @@ using namespace std;
 WellsFargoBank::WellsFargoBank()
     :name{""}, interestRate{0.0}, balance{0.0}, loans{0.0}
 {
-
+   
 }
 
 WellsFargoBank::WellsFargoBank(string _name, double _interestRate, double _balance, double _loans)
     :name{"Wells Fargo"}, interestRate{_interestRate}, balance{_balance}, loans{_loans}
 {
-    
+
 }
